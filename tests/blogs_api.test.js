8675b2c4848d79ddde7331031757c61d6ddebedc
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-21 16:43:00 +0200

    4.9

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -2,13 +2,47 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe("GET tests", () => {
+
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+})
+
+describe("POST tests", () => {
+
+    test("blog is added to database", async () => {
+        const newBlog = {
+            title: "test",
+            author: "tester",
+            url: "test.com"
+        }
+
+        await api
+            .post("/api/blogs")
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const response = await api
+            .get('/api/blogs')
+
+        const contents = response.body.map(r => r.title)
+
+
+        expect(contents).toContain('test')
+    
+})
+    
+
+
+
 })
 
 afterAll(() => {
-  server.close()
+    server.close()
 })
