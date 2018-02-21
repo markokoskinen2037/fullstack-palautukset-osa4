ba23131707cc2cac18131f9622abe0df9cb68e68
Marko Koskinen <markokoskinen2037@gmail.com>
2018-02-21 16:22:33 +0200

    4.8

--- index.js
+++ index.js
@@ -4,30 +4,32 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
 const middleware = require('./utils/middleware')
-const blogsRouter = require("./controllers/blogs")
+const blogsRouter = require('./controllers/blogs')
+const config = require('./utils/config')
 
-
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
-
+mongoose.connect(config.mongoUrl)
+mongoose.Promise = global.Promise
 
 app.use(cors())
 app.use(bodyParser.json())
-app.use(express.static("build"))
+app.use(express.static('build'))
 app.use(middleware.logger)
 
-app.use("/api/blogs", blogsRouter)
-
-const mongoUrl = 'mongodb://admin:salasana@ds229448.mlab.com:29448/fullstack2037'
-mongoose.connect(mongoUrl)
-mongoose.Promise = global.Promise
+app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.error)
 
-const PORT = 3003
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+const server = http.createServer(app)
+
+server.listen(config.port, () => {
+  console.log(`Server running on port ${config.port}`)
 })
+
+server.on('close', () => {
+  mongoose.connection.close()
+})
+
+module.exports = {
+  app, server
+}
